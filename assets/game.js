43b9078b29df3f097effa85f94d773fd16945222
Daniel Mayer <danmayercraigs@gmail.com>
2020-03-10 22:49:26 -0400

    further tightening, everything still functioning

--- assets/game.js
+++ assets/game.js
@@ -28,148 +28,125 @@ var wrongLetters = [];
 // Game counters
 var victories = 0;
 var defeats = 0;
-var numGuesses = 9;
+var remaining = 9;
 
-// FUNCTIONS (These are bits of code that we will call upon to run when needed)
-// =========================================================================================
+// prepares the game for gameplay, doesn't initiate it.
+function gameStart() {
+  // resets remaining to 9
+  remaining = 9;
 
-// startGame()
-// Its how we we will start and restart the game.
-// (Note: It's not being run here. It's just being made for future use.)
-function startGame() {
-  // Reset the guesses back to 0.
-  numGuesses = 9;
-
-  // Solution is chosen randomly from wordList.
+  // Selects the word from the answerarray randomly
   selectedAnswer = answerarray[Math.floor(Math.random() * answerarray.length)];
-  // The word is broken into individual letters.
+  //breaks the answer down into individual letters and stores them in the array
   answerLetterArray = selectedAnswer.split("");
-  // We count the number of letters in the word.
+  //counts the length of the array to get the number of letters to create dashes with
   dashCount = answerLetterArray.length;
 
-  // We print the solution in console (for testing).
-  console.log(selectedAnswer);
+  // Console logs the selected word for testing purpose
+  // console.log(selectedAnswer);
 
-  // CRITICAL LINE - Here we *reset* the guess and success array at each round.
+  // emptys the dashes and letters array for the next round
   dashesAndLetters = [];
-  // CRITICAL LINE - Here we *reset* the wrong guesses from the previous round.
+  // emptys the wrong letters array for the next round
   wrongLetters = [];
 
-  // Fill up the dashesAndLetters list with appropriate number of blanks.
-  // This is based on number of letters in solution.
+  // for loop to cycle through the dash count and holds them in dashes and letters array
   for (var i = 0; i < dashCount; i++) {
     dashesAndLetters.push("_");
   }
 
-  // Print the initial blanks in console.
-  console.log(dashesAndLetters);
+  // console log the dashes and letters for testing
+  // console.log(dashesAndLetters);
 
-  // Reprints the guessesLeft to 9
-  document.getElementById("guesses-left").innerHTML = numGuesses;
+  //sets the remaining guesses back to 9
+  document.getElementById("guesses-left").innerHTML = remaining;
 
-  // Prints the blanks at the beginning of each round in the HTML
+  // displays the blanks to the inner html for the next round
   document.getElementById("word-blanks").innerHTML = dashesAndLetters.join(" ");
 
-  // Clears the wrong guesses from the previous round
+  // clears the previous user selected letters
   document.getElementById("wrong-guesses").innerHTML = wrongLetters.join(" ");
 }
 
-// checkLetters() function
-// It's where we will do all of the comparisons for matches.
-// Again, it's not being called here. It's just being made for future use.
-function checkLetters(letter) {
+function verifyLetters(letter) {
   // This boolean will be toggled based on whether or not a user letter is found anywhere in the word.
-  var letterInWord = false;
+  var userLetter = false;
 
-  // Check if a letter exists inside the array at all.
+  // for loop to cycle through and see if the users letter exists in the selected answer array
   for (var i = 0; i < dashCount; i++) {
     if (selectedAnswer[i] === letter) {
-      // If the letter exists then toggle this boolean to true. This will be used in the next step.
-      letterInWord = true;
+      // if the letter exists in the selected answer then the user letter will be set to true.
+      userLetter = true;
     }
   }
 
-  // If the letter exists somewhere in the word, then figure out exactly where (which indices).
-  if (letterInWord) {
-    // Loop through the word.
+  // if letter exists in the answer this will indicate which indices the letter falls in
+  if (userLetter) {
     for (var j = 0; j < dashCount; j++) {
-      // Populate the dashesAndLetters with every instance of the letter.
+      // everywhere in the dashcount that the userletter is a match it will update the dashes and letters array.
       if (selectedAnswer[j] === letter) {
-        // Here we set the specific space in blanks and letter equal to the letter when there is a match.
         dashesAndLetters[j] = letter;
       }
     }
-    // Logging for testing.
+    // console log the dashes and letters for testing
     console.log(dashesAndLetters);
   }
-  // If the letter doesn't exist at all...
+  // else if the letter is not found in the answer the letter is displayed in the wrong letters and the remaining count is decreased.
   else {
-    // ..then we add the letter to the list of wrong letters, and we subtract one of the guesses.
     wrongLetters.push(letter);
-    numGuesses--;
+    remaining--;
   }
 }
 
-// roundComplete() function
-// Here we will have all of the code that needs to be run after each guess is made
-function roundComplete() {
-  // First, log an initial status update in the console telling us how many wins, losses, and guesses are left.
-  console.log(
-    "WinCount: " +
-      victories +
-      " | LossCount: " +
-      defeats +
-      " | NumGuesses: " +
-      numGuesses
-  );
-
-  // Update the HTML to reflect the new number of guesses. Also update the correct guesses.
-  document.getElementById("guesses-left").innerHTML = numGuesses;
-  // This will print the array of guesses and blanks onto the page.
+// Function to execute after each guess
+function nextGuess() {
+  // updates the remaining guess count
+  document.getElementById("guesses-left").innerHTML = remaining;
+  //updates the dashes and letters displayed
   document.getElementById("word-blanks").innerHTML = dashesAndLetters.join(" ");
-  // This will print the wrong guesses onto the page.
+  // updates the wrong letters displayed
   document.getElementById("wrong-guesses").innerHTML = wrongLetters.join(" ");
 
-  // If we have gotten all the letters to match the solution...
+  // If answer is solved by all of the letters matching....
   if (answerLetterArray.toString() === dashesAndLetters.toString()) {
-    // ..add to the win counter & give the user an alert.
+    // increase victory counter and alert user of victory
     victories++;
     alert("You win!");
 
-    // Update the win counter in the HTML & restart the game.
+    // display new victory count and reset with the gameStart function
     document.getElementById("win-counter").innerHTML = victories;
-    startGame();
+    gameStart();
   }
 
-  // If we've run out of guesses..
-  else if (numGuesses === 0) {
+  // If remaining runs out and all letters arent matching add to the defeat counter and alert user
+  else if (remaining === 0) {
     // Add to the loss counter.
     defeats++;
     // Give the user an alert.
     alert("You lose");
 
-    // Update the loss counter in the HTML.
+    // display new defeat count and reset with the gameStart function
     document.getElementById("loss-counter").innerHTML = defeats;
     // Restart the game.
-    startGame();
+    gameStart();
   }
 }
 
 // MAIN PROCESS (THIS IS THE CODE THAT CONTROLS WHAT IS ACTUALLY RUN)
 // ==================================================================================================
 
-// Starts the Game by running the startGame() function
-startGame();
+// has the game prepared for start after loading
+gameStart();
 
-// Then initiate the function for capturing key clicks.
+// game actually starts on users first key click
 document.onkeyup = function(event) {
-  // Check if the key pressed is a letter.
+  // verifies that the key pressed is a letter and not a number
   if (event.keyCode >= 65 && event.keyCode <= 90) {
     // Converts all key clicks to lowercase letters.
     var letterGuessed = event.key.toLowerCase();
     // Runs the code to check for correctness.
-    checkLetters(letterGuessed);
+    verifyLetters(letterGuessed);
     // Runs the code after each round is done.
-    roundComplete();
+    nextGuess();
   }
 };
